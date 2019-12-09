776ba7820f0c6be9096427ba141f64fcfcbae052
deepa <deepaa24k@gmail.com>
2019-12-09 14:50:01 +0530

    arrayclass file

---
+++ backend/arrayclass/Array3.java
@@ -0,0 +1,11 @@
+
+public class Array3 {
+public static void main(String[] args) {
+	char[] c1= new char[4];
+	c1[0]='c';
+	c1[3]='a';
+	for(int i=0;i<c1.length;i++) {
+		System.out.println(c1[i]);
+	}
+}
+}
